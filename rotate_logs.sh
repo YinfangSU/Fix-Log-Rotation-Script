3df68d85f0ca73677f2202f56c1dab420e0d84cd
YinfangSU <suyinfang678@outlook.com>
2026-09-12 16:37:56 +1000

    Fix the Bug 2

--- rotate_logs.sh
+++ rotate_logs.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-
 archive_dir= $1
-log_dir=$2for f in $(ls $log_dir/*.log); do
+log_dir=$2
+# Check if the correct number of arguments is provided
+if [[ $# -ne 2]]; then
+  # Print usage message to stderr 
+  echo "Usage: $0 <archive_dir> <log_dir>" >$2
+  exit 1
+fi
+
+for f in "$log_dir"/*.log; do
   age=$(find $f -mtime +7)if [ $age ]; thenmv $f $archive_dir/
     count=$count+1fidoneecho "Archived $count files"
